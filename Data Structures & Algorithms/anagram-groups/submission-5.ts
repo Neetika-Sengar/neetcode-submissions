0f78f1994dc710121c432999d3f23ecf61693622
Neetika-Sengar <115067888+Neetika-Sengar@users.noreply.github.com>
2026-07-16 20:25:09 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const hashMap = new Map<string,string[]>
+
+        for (let item of strs) {
+            const sortKey = new Array(26).fill(0)
+            // const sortedKey = item.split('').sort().join('')
+            
+
+            for (let i=0; i<item.length; i++){
+                const j = item.charCodeAt(i)-'a'.charCodeAt(0)
+                sortKey[j] =  sortKey[j] + 1
+            }
+          const sortedKey = sortKey.join('#')
+
+            if(hashMap.has(sortedKey)){
+                hashMap.set(sortedKey, [... hashMap.get(sortedKey), item])
+            } 
+            else {
+                hashMap.set(sortedKey,[item])
+            }
+        }
+        
+        return Array.from(hashMap.values())
+    }
+}
