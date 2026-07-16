eb5aa27a807ebc4027b21053fa2bf931bd4b8d44
Neetika-Sengar <115067888+Neetika-Sengar@users.noreply.github.com>
2026-07-16 09:37:01 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length!==t.length){
+            return false
+        }
+
+        const sortFn = (str:string)=>str?.trim()?.split('')?.sort()?.join()
+
+        const sortedS= sortFn(s)
+        const sortedT = sortFn(t)
+        
+        return sortedS === sortedT
+    }
+}
