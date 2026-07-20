60eaa448e591b7dcce1c232c45f3a6b9240856b1
Neetika-Sengar <115067888+Neetika-Sengar@users.noreply.github.com>
2026-07-20 22:02:11 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const hashMap= new Map<number, number>()
+
+        for(let item of nums){
+            if(hashMap.get(item)){
+                hashMap.set(item, hashMap.get(item)+1)
+            } else {           
+                hashMap.set(item,1)
+                }
+        }
+
+        const mostFrequentedElements = ([...hashMap.entries()].sort((a,b)=>b[1] - a[1])).slice(0,k).map((item)=> item[0])
+        return mostFrequentedElements
+    }
+}
