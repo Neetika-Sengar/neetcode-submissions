bd4485a1497ff1af0e35d419749a18bbd77337a8
Neetika-Sengar <115067888+Neetika-Sengar@users.noreply.github.com>
2026-08-03 17:10:43 +0530

    Add: three-integer-sum - submission-11

---
+++ Data Structures & Algorithms/three-integer-sum/submission-11.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        nums.sort((a,b)=> a-b)
+        let n = nums.length
+        const answer = []
+        if(!n)return[]
+        
+        for(let i = 0; i<n; i++ ){
+            let l = i +1
+            let r = n-1
+            if(nums[i]==nums[i-1])continue
+            
+            while (l<r){
+                const sum = nums[i]+nums[l]+nums[r]
+                if(sum===0){
+                    answer.push([nums[i],nums[l],nums[r]])
+                    l++
+                    r--
+                    while(l<r && nums[l]==nums[l-1]){
+                        l++
+                    }
+                } else if (sum<0){
+                    l++
+                } else {r--}
+               
+                
+            }
+        }
+        return answer
+    }
+}
